c3a0f9884fb421edd36d09c45093c123b2bc2505
sooyun3451 <sooyun020820@gmail.com>
2025-07-08 16:50:07 +0900

    feat)게시글 검색 결과 목록

--- js/reviews_list.js
+++ js/reviews_list.js
@@ -1,1 +1,14 @@
 // reviews_list.js
+const hearts = document.querySelectorAll('.favo');
+
+hearts.forEach((heart) => {
+  heart.addEventListener('click', () => {
+    heart.classList.toggle('on');
+  })
+});
+
+if(heart.classList.contains('on')) {
+  heart.classList.remove('on');
+}else {
+  heart.classList.add('on');
+}
