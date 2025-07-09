0c0e2dcb6e89732b3e3a4618d877f3acd55e4505
sooyun3451 <sooyun020820@gmail.com>
2025-07-09 16:40:24 +0900

    어드민 페이지

---
+++ js/admin.js
@@ -0,0 +1,16 @@
+const pages = document.querySelectorAll('.page');
+const pageBtns = document.querySelectorAll('.page-btn')
+
+pages.forEach(page => {
+  page.addEventListener('click', () => {
+    pages.forEach((p) => p.classList.remove('active'));
+    page.classList.add('active');
+  });
+});
+
+pageBtns.forEach(page => {
+  page.addEventListener('click', () => {
+    pageBtns.forEach((p) => p.classList.remove('active'));
+    page.classList.add('active')
+  });
+})
--- js/reviews_list.js
+++ js/reviews_list.js
@@ -4,11 +4,5 @@ const hearts = document.querySelectorAll('.favo');
 hearts.forEach((heart) => {
   heart.addEventListener('click', () => {
     heart.classList.toggle('on');
-  })
+  });
 });
-
-if(heart.classList.contains('on')) {
-  heart.classList.remove('on');
-}else {
-  heart.classList.add('on');
-}
