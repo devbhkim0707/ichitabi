b2509572f178dddb63f16571a3848b744567ffbf
sooyun3451 <sooyun020820@gmail.com>
2025-07-11 16:07:36 +0900

    feat)해시태그 탭 & 더보기

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ const regionKanto = document.getElementsByClassName('kiqJ8lXv0XRSXSa_ThjML')[2];
 regionKanto.addEventListener('click', () => {
   window.location.href = './reviews/map.html';
 });
+
+const hashs = document.querySelectorAll('#hash-div p');
+const seasons = document.querySelectorAll('#hash-season p');
+
+hashs.forEach((hash, i) => {
+  hash.addEventListener('click', () => {
+    hashs.forEach((review) => {
+      review.classList.remove('active');
+    });
+    hashs[i].classList.add('active');
+  });
+});
+
+seasons.forEach((season, i) => {
+  season.addEventListener('click', () => {
+    seasons.forEach(festival => {
+      festival.classList.remove('active');
+    });
+    seasons[i].classList.add('active');
+  });
+});
