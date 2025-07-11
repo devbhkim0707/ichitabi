2a2fa787705146cc5e8619fbffe5074e341de81a
jihyeon <jh103177@naver.com>
2025-07-11 18:27:40 +0900

    feat)하트 카운터  수정

--- js/review_detail.js
+++ js/review_detail.js
@@ -1,7 +1,14 @@
 // review_detail.js
 
 const likeBtn = document.getElementById('like-btn');
+const likeCount = document.getElementById('like-count');
+let count = 0;
 
 likeBtn.addEventListener('click', () => {
   likeBtn.classList.toggle('on');
+  if (likeBtn.classList.contains('on')) {
+    likeCount.innerHTML = ++count;
+  } else {
+    likeCount.innerHTML = --count;
+  }
 });
