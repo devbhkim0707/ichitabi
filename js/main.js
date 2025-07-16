fbe7f25a99bbf8339f8e551780f4c3c211f4a4fe
devbhkim0707 <devbhkim0707@gmail.com>
2025-07-16 17:12:29 +0900

    feat) 축제 영역 캐러셀 구현 및 헤더 드롭다운 메뉴 수정

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // main.js
 isMainPage = true;
 
+// 간토 지방 클릭 이벤트 href
 const regionKanto = document.getElementsByClassName('kiqJ8lXv0XRSXSa_ThjML')[2];
 
 regionKanto.addEventListener('click', () => {
@@ -21,9 +22,64 @@ hashs.forEach((hash, i) => {
 
 seasons.forEach((season, i) => {
   season.addEventListener('click', () => {
-    seasons.forEach(festival => {
+    seasons.forEach((festival) => {
       festival.classList.remove('active');
     });
     seasons[i].classList.add('active');
   });
 });
+
+// 축제 영역 캐러셀
+const festivalNextBtn = document.getElementById('btn-next-festival');
+const festivalPrevBtn = document.getElementById('btn-prev-festival');
+const festivalsListUl = document.querySelector('#festivals-list ul');
+
+let currentIndex = 0;
+
+festivalNextBtn.addEventListener('click', () => {
+  currentIndex = slide(festivalsListUl, 'left', currentIndex);
+});
+
+festivalPrevBtn.addEventListener('click', () => {
+  currentIndex = slide(festivalsListUl, 'right', currentIndex);
+});
+
+function slide(element, direction, index) {
+  const slideWidth = 300;
+  const transitionSpeed = 300;
+  const elementLength = element.querySelectorAll('li').length;
+
+  const visibleItems = 4;
+
+  if (direction === 'left') {
+    if (index === elementLength - visibleItems) {
+      setTimeout(() => {
+        bounce(direction), 200;
+      });
+    } else {
+      index++;
+    }
+  } else if (direction === 'right') {
+    if (index === 0) {
+      setTimeout(() => {
+        bounce(direction), 200;
+      });
+    } else {
+      index--;
+    }
+  }
+  setTimeout(() => {
+    element.style.transform = `translateX(${-(slideWidth * index)}px)`;
+    element.style.transition = `all ${transitionSpeed}ms ease`;
+  }, 100);
+  return index;
+
+  function bounce(direction) {
+    if (direction === 'left') {
+      element.style.transform = `translateX(${-(slideWidth * index - 100)}px)`;
+    } else if (direction === 'right') {
+      element.style.transform = `translateX(${-(slideWidth * index + 100)}px)`;
+    }
+    element.style.transition = `all ${transitionSpeed}ms ease`;
+  }
+}
